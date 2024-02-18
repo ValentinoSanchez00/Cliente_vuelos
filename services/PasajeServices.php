62abc8a3710789ae20779cdbb7dd18caba6955af
valen <valen@192.168.1.18>
2024-02-18 16:16:26 +0100

    metodo de actualizar en servicio

--- services/PasajeServices.php
+++ services/PasajeServices.php
@@ -45,7 +45,7 @@ class PasajeServices {
     
         public function validar($cod, $identificador, $numasiento, $clase, $pvp) {
         $envio = json_encode(array("pasajerocod" => $cod, "identificador" => $identificador, "numasiento" => $numasiento, "clase" => $clase, "pvp" => $pvp));
-        $urlmiservicio = "http://localhost/_servWeb/vueloservice/pasaje.php/";
+        $urlmiservicio = "http://localhost/_servWeb/vueloservice/pasaje.php/?validar";
         $conexion = curl_init();
         curl_setopt($conexion, CURLOPT_URL, $urlmiservicio);
         //Cabecera, tipo de datos y longitud de envío
@@ -64,7 +64,27 @@ class PasajeServices {
         }
         curl_close($conexion);
     }
-    
+            public function actualizar($id,$cod, $identificador, $numasiento, $clase, $pvp) {
+        $envio = json_encode(array("pasajerocod" => $cod, "identificador" => $identificador, "numasiento" => $numasiento, "clase" => $clase, "pvp" => $pvp));
+        $urlmiservicio = "http://localhost/_servWeb/vueloservice/pasaje.php/?actualizar=".$id;
+        $conexion = curl_init();
+        curl_setopt($conexion, CURLOPT_URL, $urlmiservicio);
+        //Cabecera, tipo de datos y longitud de envío
+        curl_setopt($conexion, CURLOPT_HTTPHEADER,
+                array('Content-type: application/json', 'Content-Length: ' . mb_strlen($envio)));
+        //Tipo de petición
+        curl_setopt($conexion, CURLOPT_POST, TRUE);
+        //Campos que van en el envío
+        curl_setopt($conexion, CURLOPT_POSTFIELDS, $envio);
+        //para recibir una respuesta
+        curl_setopt($conexion, CURLOPT_RETURNTRANSFER, true);
+
+        $res = curl_exec($conexion);
+        if ($res) {
+            return $res;
+        }
+        curl_close($conexion);
+    }
 
 
 }
