91ad073fced4bd11f1272d69df1c2e16c289d026
ValentinoSanchez00 <sanchezravertavalentino@gmail.com>
2024-02-19 19:18:33 +0100

    ventana modal añadida

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
     <head>
         <title>Mis Departamentos</title>
         <link rel="stylesheet" href="./css/style.css"/>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     </head>
     <body>
         <?php
 // Incluye el archivo frontcontroller.php
         include 'frontcontroller.php';
         ?>
-        
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
 </html>
 
--- views/PasajeView.php
+++ views/PasajeView.php
@@ -30,7 +30,29 @@ class PasajeView {
                             <td><?= $pasaje->getPvp() ?></td>
                             <td>
                                 <a class="btn btn-success " href="./index.php?controller=Pasaje&action=FormEdicion&id=<?php echo $pasaje->getIdpasaje() ?>">Editar</a>
-                                <a class="btn btn-danger " href="./index.php?controller=Pasaje&action=mostrarInicio&id=<?php echo $pasaje->getIdpasaje() ?>">Borrar</a>
+                                <!-- Button trigger modal -->
+                                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal_<?php echo $pasaje->getIdpasaje()?>">
+                                   Borrar
+                                </button>
+
+                                <!-- Modal -->
+                                <div class="modal fade" id="exampleModal_<?php echo $pasaje->getIdpasaje()?>" tabindex="-1" aria-labelledby="exampleModalLabel_<?php echo $pasaje->getIdpasaje()?>" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="exampleModalLabel_<?php echo $pasaje->getIdpasaje()?>">Eliminar Pasaje</h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                ¿Estas seguro de eliminar el pasaje: <?php echo $pasaje->getIdpasaje(); ?> ?
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                                <button type="button" class="btn btn-danger">Borrar</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
 
                             </td>
                         </tr>
@@ -75,7 +97,28 @@ class PasajeView {
                             <td><?= $pasaje->getPvp() ?></td>
                             <td>
                                 <a class="btn btn-success " href="./index.php?controller=Pasaje&action=FormEdicion&id=<?php echo $pasaje->getIdpasaje() ?>">Editar</a>
-                                <a class="btn btn-danger " href="./index.php?controller=Pasaje&action=mostrarInicio&id=<?php echo $pasaje->getIdpasaje() ?>">Borrar</a>
+                                  <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal_<?php echo $pasaje->getIdpasaje()?>">
+                                   Borrar
+                                </button>
+
+                                <!-- Modal -->
+                                <div class="modal fade" id="exampleModal_<?php echo $pasaje->getIdpasaje()?>" tabindex="-1" aria-labelledby="exampleModalLabel_<?php echo $pasaje->getIdpasaje()?>" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="exampleModalLabel_<?php echo $pasaje->getIdpasaje()?>">Eliminar Pasaje</h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                ¿Estas seguro de eliminar el pasaje: <?php echo $pasaje->getIdpasaje(); ?> ?
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                                <button type="button" class="btn btn-danger">Borrar</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </td>
                         </tr>
                     <?php endforeach; ?>
@@ -91,7 +134,7 @@ class PasajeView {
         ?>
         <div class="container mt-4 transparent-white-bg">
             <h2>Formulario de Actualización de Pasaje: <?php echo $id ?></h2>
-            <form action="./index.php?controller=Pasaje&action=update&id=<?php echo $id?>" method="POST">
+            <form action="./index.php?controller=Pasaje&action=update&id=<?php echo $id ?>" method="POST">
                 <input type="hidden" name="idPasaje" value="<?php echo $id ?>">
 
                 <div class="form-group">
