8fd26e0c3542c4bf73b206cdf8b5287d3d908dab
valen <valen@192.168.1.18>
2024-02-20 13:01:16 +0100

    haciendo fomulario

--- controllers/VuelosController.php
+++ controllers/VuelosController.php
@@ -1,7 +1,9 @@
 <?php
 
 require 'services/Vuelo.php';
+
 class VuelosController {
+
     // Obtiene una instancia del modelo y de la vista de tareas
     private $service;
     private $view;
@@ -10,64 +12,64 @@ class VuelosController {
         $this->service = new VuelosServices();
         $this->view = new VuelosView();
     }
-    
+
     public function verInicio() {
         $this->view->mostrarInicio();
     }
-    
-    
-    
-  public function verTodos() {
-     $vuelos = json_decode($this->service->request_curl(), true);
-    $arrayDeVuelos = [];
 
-    foreach ($vuelos as $valoresVuelo) {
-        $aeropuertoOrigen = new Aeropuerto($valoresVuelo["codigo_origen"], $valoresVuelo["nombre_origen"],null,null);
-        $aeropuertoDestino = new Aeropuerto($valoresVuelo["codigo_destino"], $valoresVuelo["nombre_destino"],null,null);
+    public function verTodos() {
+        $vuelos = json_decode($this->service->request_curl(), true);
+        $arrayDeVuelos = [];
 
-        $nuevoVuelo = new Vuelo(
-            $valoresVuelo["identificador"],
-            $aeropuertoOrigen,
-            $aeropuertoDestino,
-            $valoresVuelo["tipovuelo"],
-            $valoresVuelo["fechavuelo"],
-            $valoresVuelo["descuento"],
-            $valoresVuelo["numpasajero"]
-        );
+        foreach ($vuelos as $valoresVuelo) {
+            $aeropuertoOrigen = new Aeropuerto($valoresVuelo["codigo_origen"], $valoresVuelo["nombre_origen"], null, null);
+            $aeropuertoDestino = new Aeropuerto($valoresVuelo["codigo_destino"], $valoresVuelo["nombre_destino"], null, null);
 
-        array_push($arrayDeVuelos, $nuevoVuelo);
+            $nuevoVuelo = new Vuelo(
+                    $valoresVuelo["identificador"],
+                    $aeropuertoOrigen,
+                    $aeropuertoDestino,
+                    $valoresVuelo["tipovuelo"],
+                    $valoresVuelo["fechavuelo"],
+                    $valoresVuelo["descuento"],
+                    $valoresVuelo["numpasajero"]
+            );
+
+            array_push($arrayDeVuelos, $nuevoVuelo);
+        }
+
+        $this->view->mostrartodos($arrayDeVuelos);
     }
-
-    $this->view->mostrartodos($arrayDeVuelos);
-}
 
     public function mostrarFormulario1vuelo() {
-        $this->view->formularioporId();
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $this->view->opcionesporId($_POST["select1"]);
+        } else{
+            $identificadores = json_decode($this->service->request_curlIdentificadores(), true);
+            $arraydeidentificadores = [];
+            foreach ($identificadores as $valores) {
+            array_push($arraydeidentificadores, $valores["identificadores"]);
+        }
+
+
+
+        $this->view->formularioporId($arraydeidentificadores);
+        }
     }
-    
+
     public function mostrarporId() {
-        $identificador=$_POST["identificador"];
-        $Vuelos= json_decode($this->service->request_curlId($identificador),true);
-           $arraydeVuelos=[];
+        $identificador = $_POST["identificador"];
+        $Vuelos = json_decode($this->service->request_curlId($identificador), true);
+        $arraydeVuelos = [];
         foreach ($Vuelos as $valoresVuelo) {
-          
-          $nuevoVuelo=new Vuelo($valoresVuelo["identificador"],$valoresVuelo["aeropuertoorigen"],$valoresVuelo["aeropuertodestino"],$valoresVuelo["tipovuelo"],$valoresVuelo["fechavuelo"],$valoresVuelo["descuento"],$valoresVuelo["numpasajero"]);
-          
-          array_push($arraydeVuelos,$nuevoVuelo);
 
-      }
-      
-      
-      
-      $this->view->mostrartodos($arraydeVuelos);
+            $nuevoVuelo = new Vuelo($valoresVuelo["identificador"], $valoresVuelo["aeropuertoorigen"], $valoresVuelo["aeropuertodestino"], $valoresVuelo["tipovuelo"], $valoresVuelo["fechavuelo"], $valoresVuelo["descuento"], $valoresVuelo["numpasajero"]);
+
+            array_push($arraydeVuelos, $nuevoVuelo);
+        }
+
+
+
+        $this->view->mostrartodos($arraydeVuelos);
     }
-    
-    
-    
-
-   
-    
-    
-    
-    
 }
--- views/VuelosView.php
+++ views/VuelosView.php
@@ -13,70 +13,97 @@ class VuelosView {
             <a href="./index.php?controller=Vuelos&action=mostrarFormulario1vuelo">Mostrar 1 Vuelo</a>
             <a href="./index.php?controller=General&action=mostrarformularioPasaje">Insertar un pasaje</a>
             <a href="./index.php?controller=Pasaje&action=mostrarPasajes">Ver todos los pasajes</a>
-            
+
         </div>
 
         <?php
     }
 
-public function mostrartodos($array) {
-    ?>
-    <h1>Todos los Vuelos</h1>
+    public function mostrartodos($array) {
+        ?>
+        <h1>Todos los Vuelos</h1>
 
-    <!-- INICIO TABLA -->
-    <div class="table-container">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Identificador</th>
-                    <th>Aeropuerto Origen</th>
-                    <th>Nombre Origen</th>
-                    <th>Aeropuerto Destino</th>
-                    <th>Nombre Destino</th>
-                    <th>Tipo de Vuelo</th>
-                    <th>Num Pasajeros</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                foreach ($array as $vuelo) {
-                    ?>
+        <!-- INICIO TABLA -->
+        <div class="table-container">
+            <table class="table">
+                <thead>
                     <tr>
-                        <td><?php echo $vuelo->getIdentificador(); ?></td>
-                        <td><?php echo $vuelo->getAeropuertoOrigen()->getCodigo(); ?></td>
-                        <td><?php echo $vuelo->getAeropuertoOrigen()->getNombre(); ?></td>
-                        <td><?php echo $vuelo->getAeropuertoDestino()->getCodigo(); ?></td>
-                        <td><?php echo $vuelo->getAeropuertoDestino()->getNombre(); ?></td>
-                        <td><?php echo $vuelo->getTipovuelo(); ?></td>
-                        <td><?php echo $vuelo->getNumpasajero(); ?></td>
+                        <th>Identificador</th>
+                        <th>Aeropuerto Origen</th>
+                        <th>Nombre Origen</th>
+                        <th>Aeropuerto Destino</th>
+                        <th>Nombre Destino</th>
+                        <th>Tipo de Vuelo</th>
+                        <th>Num Pasajeros</th>
                     </tr>
+                </thead>
+                <tbody>
                     <?php
-                }
-                ?>
-            </tbody>
-        </table>
+                    foreach ($array as $vuelo) {
+                        ?>
+                        <tr>
+                            <td><?php echo $vuelo->getIdentificador(); ?></td>
+                            <td><?php echo $vuelo->getAeropuertoOrigen()->getCodigo(); ?></td>
+                            <td><?php echo $vuelo->getAeropuertoOrigen()->getNombre(); ?></td>
+                            <td><?php echo $vuelo->getAeropuertoDestino()->getCodigo(); ?></td>
+                            <td><?php echo $vuelo->getAeropuertoDestino()->getNombre(); ?></td>
+                            <td><?php echo $vuelo->getTipovuelo(); ?></td>
+                            <td><?php echo $vuelo->getNumpasajero(); ?></td>
+                        </tr>
+                        <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
 
-        <a href="./index.php?controller=Vuelos&action=mostrarInicio">Volver</a>
-    </div>
+            <a href="./index.php?controller=Vuelos&action=mostrarInicio">Volver</a>
+        </div>
 
-    <!-- FIN TABLA -->
-    <?php
-}
+        <!-- FIN TABLA -->
+        <?php
+    }
 
-
-    public function formularioporId() {
+    public function formularioporId($identificadores) {
         ?>
         <body>
             <div class="container">
                 <h1>Formulario por Identificador</h1>
-                <form action="./index.php?controller=Vuelos&action=mostrarporId" method="post">
-                    <label for="identificador">Identificador:</label>
-                    <input type="text" id="identificador" name="identificador">
+                <form action="./index.php?controller=Vuelos&action=mostrarFormulario1vuelo" method="post">
+                    <div class="mb-3">
+                        <label for="select1" class="form-label">Selecciona Identificador de vuelo:</label>;
+                        <select id="select1" name="select1" class="form-select">;
+                            <?php
+                            foreach ($identificadores as $id) {
+                                echo '<option value="' . $id . '">' . $id . '</option>';
+                            }
+                            ?>
+                        </select>';
+                    </div>;
 
                     <button type="submit">Enviar</button>
                 </form>
             </div>
         </body>
+        <?php
+    }
+
+    public function opcionesporId($id) {
+        ?>
+        <div class="container p-5">
+            <h1 class="text-center mb-4">Formulario por Identificador: <?php echo $id; ?></h1>
+
+            <div class="d-flex justify-content-center">
+                <!-- Primer formulario -->
+                <form class="me-5" action="./index.php?controller=Vuelos&action=mostrarFormulario1vuelo" method="post">
+                    <button type="submit" class="btn btn-primary">Vuelo</button>
+                </form>
+
+                <!-- Segundo formulario -->
+                <form action="./index.php?controller=Vuelos&action=mostrarFormulario1vuelo" method="post">
+                    <button type="submit" class="btn btn-primary">Pasaje</button>
+                </form>
+            </div>
+        </div>
         <?php
     }
 }
